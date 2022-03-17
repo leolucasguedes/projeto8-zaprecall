ef166170f7e174496ecc8f9a9993ca6ae00ef7ea
Leonardo Lucas <leolucasguedes@yahoo.com.br>
2022-03-17 13:20:40 -0300

    Commit inicial

---
+++ src/components/Cards/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 1.5em;
+  color: green;
+`;
+
+export const Button = styled.button`
+  color: red;
+  background-color: orange;
+`;
